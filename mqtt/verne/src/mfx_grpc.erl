023cbaada9dd947223458d96c5e3d17857580ee8
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2019-10-27 02:50:21 +0100

    Optimize grpc gen_server
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- mqtt/verne/src/mfx_grpc.erl
+++ mqtt/verne/src/mfx_grpc.erl
@@ -11,12 +11,13 @@
     terminate/2
 ]).
 
--record(state, {pool_size}).
+-record(state, {opts}).
 
 init(_Args) ->
     error_logger:info_msg("mfx_grpc genserver has started (~w)~n", [self()]),
     [{_, PoolSize}] = ets:lookup(mfx_cfg, pool_size),
-    {ok, #state{pool_size = PoolSize}}.
+    Opts = #{channel => list_to_atom("channel_" ++ integer_to_list(rand:uniform(PoolSize)))},
+    {ok, #state{opts = Opts}}.
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -24,9 +25,8 @@ start_link() ->
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
 
-handle_call({identify, Message}, _From, #state{pool_size = PoolSize} = State) ->
+handle_call({identify, Message}, _From, #state{opts = Opts} = State) ->
     error_logger:info_msg("mfx_grpc message: ~p", [Message]),
-    Opts = #{channel => list_to_atom("channel_" ++ integer_to_list(rand:uniform(PoolSize)))},
     {ok, Resp, HeadersAndTrailers} = mainflux_things_service_client:identify(Message, Opts),
     case maps:get(<<":status">>, maps:get(headers, HeadersAndTrailers)) of
         <<"200">> ->
@@ -35,9 +35,8 @@ handle_call({identify, Message}, _From, #state{pool_size = PoolSize} = State) ->
             {reply, {error, ErrorStatus}, State}
     end;
 
-handle_call({can_access_by_id, Message}, _From, #state{pool_size = PoolSize} = State) ->
+handle_call({can_access_by_id, Message}, _From, #state{opts = Opts} = State) ->
     error_logger:info_msg("mfx_grpc message: ~p", [Message]),
-    Opts = #{channel => list_to_atom("channel_" ++ integer_to_list(rand:uniform(PoolSize)))},
     {ok, _, HeadersAndTrailers} = mainflux_things_service_client:can_access_by_id(Message, Opts),
     error_logger:info_msg("mfx_grpc can_access_by_id() HeadersAndTrailers: ~p", [HeadersAndTrailers]),
     case maps:get(<<":status">>, maps:get(headers, HeadersAndTrailers)) of
